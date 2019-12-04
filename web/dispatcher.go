0b2376ffed7e558db19285034137f8931ee4ae4b
juan esteban colcombet <quinont@gmail.com>
2019-12-03 22:40:47 -0300

    Cambiando la version del web

--- web/dispatcher.go
+++ web/dispatcher.go
@@ -15,7 +15,7 @@ func main() {
 
 	fwd := &forwarder{"words.api.svc.cluster.local", 8080}
 	http.Handle("/words/", http.StripPrefix("/words", fwd))
-	fwd2 := &forwarder{"app2.app2.svc.cluster.local", 5000}
+	fwd2 := &forwarder{"app2", 5000}
 	http.Handle("/app2/", http.StripPrefix("/app2", fwd2))
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
